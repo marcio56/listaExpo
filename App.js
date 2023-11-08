a81f427ecb28d2429923f2153ebf9b2c6e16528c
Arthur Llevy <arthurlbvieiraa@gmail.com>
2023-11-08 20:44:13 -0300

    adicionando as alterações feitas na aula

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import AppTab from './AppTab';
+ 
 export default function App() {
   return (
-   <>
-   <AppTab/>
-   <StatusBar style='light'/>
-   </>
+    <>
+      <AppTab />
+      <StatusBar style="light" />
+    </>
   );
 }
-
--- AppTab.js
+++ AppTab.js
@@ -1,43 +1,41 @@
-import React from "react";
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-import AppForms from "./AppForms";
-import AppList from "./AppList";
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+ 
+import AppList from './AppList';
+import AppForms from './AppForms';
+ 
 const { Navigator, Screen } = createBottomTabNavigator();
-
+ 
 function AppTab() {
-    return(
-        
+    return (
         <NavigationContainer>
-        
-            <Navigator 
-            screenOptions={{
-                tabBarActiveTintColor: "#32264d",
-                tabBarInactiveTintColor: "#c1bccc",
-                tabBarActiveBackgroundColor: "#ebebf5",
-                tabBarInactiveBackgroundColor: "#fafafc",
-                tabBarLabelStyle: {
-                    fontSize: 13,
-                    position: 'absolute',
-                    top: 15,
-                    bottom: 0,
-                    left: 0,
-                    right: 0
-                },
-                tabBarIconStyle: {display: "none"}
-            }}
-            >
-                <Screen name="AppList" component={AppList} options={{ tabBarLabel:"Compras" }}/>
-                <Screen name="AppForm" component={AppForms} options={{ tabBarLabel:"Adicionar" }}/>
+            <Navigator
+                screenOptions={{
+                    tabBarActiveTintColor: "#32264d",
+                    tabBarInactiveTintColor: "#c1bccc",
+                    tabBarActiveBackgroundColor: "#ebebf5",
+                    tabBarInactiveBackgroundColor: "#fafafc",
+                    tabBarLabelStyle: {
+                        fontSize: 13,
+                        position: 'absolute',
+                        top: 15,
+                        bottom: 0,
+                        left: 0,
+                        right: 0
+                    },
+                    tabBarIconStyle: { display: "none" }
+                }}>
+                <Screen name="AppList" component={AppList} 
+                    options={{
+                        tabBarLabel: "Compras"
+                }}/>
+                <Screen name="AppForms" component={AppForms} 
+                options={{
+                    tabBarLabel: "Adicionar"
+            }}/>
             </Navigator>
         </NavigationContainer>
-    )
+    );
 }
+ 
 export default AppTab;
-
-
-
-
